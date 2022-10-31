dce0e912e3ce1c57ea686d42e58572413797593e
Julio-Martins <julio.neto@senaimt.edu.br>
2022-10-31 09:45:00 -0400

    finish frist part page timeline

--- resources/views/layouts/inc/header.blade.php
+++ resources/views/layouts/inc/header.blade.php
@@ -2,30 +2,37 @@
     <nav class="navbar navbar-expand-lg custom-bg-header">
         <div class="container">
             <a class="navbar-brand text-light" href="{{ url('/') }}"><img
-                    src="{{ asset('assets/image/logo-ICEVN.png') }}" alt="" class="img-logo custom-card-img-effect"></i></a>
+                    src="{{ asset('assets/image/logo-ICEVN.png') }}" alt=""
+                    class="img-logo custom-card-img-effect"></i></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <div class="collapse navbar-collapse justify-content-end navMobile" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item custom-card-img-effect">
-                        <a class="nav-link custom-text-nav" href="{{ route('site.books') }}"><i class="fa-solid fa-book"></i> Livros e revistas</a>
+                        <a class="nav-link custom-text-nav" href="{{ route('site.books') }}"><i
+                                class="fa-solid fa-book"></i> Livros e revistas</a>
                     </li>
                     <li class="nav-item custom-card-img-effect">
-                        <a class="nav-link custom-text-nav" href="{{ route('site.timeline') }}"><i class="fa-solid fa-calendar-days"></i> Programacoes</a>
+                        <a class="nav-link custom-text-nav" href="{{ route('site.timeline') }}"><i
+                                class="fa-solid fa-calendar-days"></i> Programacoes</a>
                     </li>
                     <li class="nav-item custom-card-img-effect">
-                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-photo-film"></i> Mideas</a>
+                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-photo-film"></i>
+                            Mideas</a>
                     </li>
                     <li class="nav-item custom-card-img-effect">
-                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-graduation-cap"></i> Estudos biblícos</a>
+                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-graduation-cap"></i>
+                            Estudos biblícos</a>
                     </li>
                     <li class="nav-item custom-card-img-effect">
-                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-circle-exclamation"></i> Quem somos</a>
+                        <a class="nav-link custom-text-nav" href="#"><i
+                                class="fa-solid fa-circle-exclamation"></i> Quem somos</a>
                     </li>
                     <li class="nav-item custom-card-img-effect">
-                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-address-book"></i> Contato</a>
+                        <a class="nav-link custom-text-nav" href="#"><i class="fa-solid fa-address-book"></i>
+                            Contato</a>
                     </li>
                 </ul>
             </div>
--- resources/views/site/timeline/index.blade.php
+++ resources/views/site/timeline/index.blade.php
@@ -5,13 +5,46 @@
 @endsection
 
 @section('content')
-    <div id="timeline" class="container-fluid py-5">
+    <div id="timeline" class="container-fluid py-5 bg-warning">
         <div class="col-12 py-5">
             <div class="row">
-                <div class="title">
+                <div class="title w-25 bg-white">
                     <h1>Programações</h1>
                 </div>
-                <!--tabela com as colunas horario e dias da semana-->
+            </div>
+        </div>
+        <!--tabela com as colunas horario e dias da semana-->
+        <div class="container">
+            <div class="col-12">
+                <div class="row">
+                    <div class="table-responsive">
+                        <table class="table table-dark table-striped">
+                            <tr>
+                                <th>Horário Inicial</th>
+                                <th>Horário Término</th>
+                                <th>Segunda</th>
+                                <th>Terça</th>
+                                <th>Quarta</th>
+                                <th>Quinta</th>
+                                <th>Sexta</th>
+                                <th>Sábado</th>
+                                <th>Domingo</th>
+                            </tr>
+                            <!--tabela com as linhas de horarios-->
+                            <tr>
+                                <td>09:00:00</td>
+                                <td>10:30:00</td>
+                                <td>N/A</td>
+                                <td>N/A</td>
+                                <td>Oracao</td>
+                                <td>N/A</td>
+                                <td>N/A</td>
+                                <td>Encontro de jovens</td>
+                                <td>Culto da Família</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
